3005b82f710b29aff247f04ff2b68b60eb153b66
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2015-01-26 18:53:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/joshheinrichs/jenkinssetup/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.joshheinrichs.jenkinssetup;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://paws.usask.ca");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void coverage(int i)
+    {
+        if(i == 1)
+        {
+            return;
+        }
+        if(i == 2)
+        {
+            return;
+        }
+        if(i == 3)
+        {
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/test/java/com/example/joshheinrichs/jenkinssetup/MainActivityIT.java
@@ -0,0 +1,33 @@
+package com.example.joshheinrichs.robotiumsetup;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.example.joshheinrichs.jenkinssetup.MainActivity;
+import com.robotium.solo.Solo;
+
+import junit.framework.Assert;
+
+public class MainActivityIT extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+
+    public MainActivityIT()
+    {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testFailure() {
+        Assert.assertTrue(false);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+}
